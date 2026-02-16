12a35ca3e3632acc9ddc0982cc57f86f2c01e7d1
rajeshgrandhi2007-lab <rajeshgrandhi2007@gmail.com>
2026-02-16 22:02:25 +0530

    UC1: print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPS Banner App
+ *
+ * UC1: Print OOPS to Console
+ *
+ * @author Rajesh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
