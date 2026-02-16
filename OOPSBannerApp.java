af7a1960704457ededf76043633cd798f8e4fa14
rajeshgrandhi2007-lab <rajeshgrandhi2007@gmail.com>
2026-02-16 22:47:02 +0530

    UC2: print OOPS banner using asterisks

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,21 @@
 /**
  * OOPS Banner App
  *
- * UC1: Print OOPS to Console
+ * UC2: Render OOPS as Banner using '*' and spaces
  *
  * @author Rajesh
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****     *****     ******     ***** ");
+        System.out.println(" *     *   *     *   *      *   *     ");
+        System.out.println(" *     *   *     *   *      *   *     ");
+        System.out.println(" *     *   *     *   ******      ***** ");
+        System.out.println(" *     *   *     *   *                * ");
+        System.out.println(" *     *   *     *   *                * ");
+        System.out.println("  *****     *****    *           ***** " );
     }
 }
