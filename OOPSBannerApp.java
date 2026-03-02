3e4b8e39e156083e164ac97d72dec1eb959fed36
rajeshgrandhi2007-lab <rajeshgrandhi2007@gmail.com>
2026-03-02 09:04:22 +0530

    UC6;Render OOPSbanner using arry and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,52 @@
 public class OOPSBannerApp {
+
+    // Helper Method for O
+    public static String getOPattern() {
+        return String.join("\n",
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** ");
+    }
+
+    // Helper Method for P
+    public static String getPPattern() {
+        return String.join("\n",
+                "**** ",
+                "*   *",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    ");
+    }
+
+    // Helper Method for S
+    public static String getSPattern() {
+        return String.join("\n",
+                " ****",
+                "*    ",
+                "*    ",
+                " *** ",
+                "    *",
+                "    *",
+                "**** ");
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(String.join("   ",
-                " ***** ", " ***** ", " ***** ", " ***** "));
+        String[] banner = {
+                getOPattern(),
+                getOPattern(),
+                getPPattern(),
+                getSPattern()
+        };
 
-        System.out.println(String.join("   ",
-                "*     *", "*     *", "*     *", "*      "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", " ***** ", " ***** "));
-
-        System.out.println(String.join("   ",
-                "*     *", "*     *", "*      ", "      *"));
-
-        System.out.println(String.join("   ",
-                " ***** ", " ***** ", "*      ", " ***** "));
+        for (String letter : banner) {
+            System.out.println(letter);
+        }
     }
 }
